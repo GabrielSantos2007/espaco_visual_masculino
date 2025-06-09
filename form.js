dc676fcba8d2e0c9ee52b1d95d6f8e01ef5a5109
Gabriel Santos de Lima <bielsdl04@hotmail.com>
2025-06-08 22:45:27 -0300

    Update form.js

--- form.js
+++ form.js
@@ -13,7 +13,7 @@ botaoEnviar.addEventListener("click", function(event){
 
 
         //Monta o texto do retorno 
-        msgRetorno=form.sobrenome.value + ", agradecemos o seu contatos com a gente! <br><br>Vamos avaliar suas considerações e retornaremos o mais rápido possível."
+        msgRetorno="<span style='color: white;'>" + form.sobrenome.value + ", agradecemos o seu contatos com a gente! <br><br>Vamos avaliar suas considerações e retornaremos o mais rápido possível."
 
         //Abre a div de retorno
         document.getElementById("formRetorno").style.display="block";
